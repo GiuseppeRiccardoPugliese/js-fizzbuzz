f862d1bf8a1d35f71aeeda0813f62025651939ac
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-11-21 15:19:59 +0100

    Fine esercizio

--- JS/script.js
+++ JS/script.js
@@ -6,27 +6,27 @@ Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i mult
 const container = document.querySelector(".container");
 
 // devo far stampare i numeri da 1 a 100
-for (i = 1; i <= 100; i++) {
-    console.log(i);
+for (let i = 1; i <= 100; i++) {
 
-    // per i multipli di 3 stampa "Fizz"
-    if (i % 3 === 0) {
-        let fizz = "Fizz";
-        console.log(fizz);
-        // i.innerHTML = "fizz"
-    } if (i % 5 === 0) {
-        let buzz = "Buzz";
-        console.log(buzz);
-    } if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) { // per i multipli di 3 e di 5 stampo "FizzBuzz"
         let fizzBuzz = "FizzBuzz";
         console.log(fizzBuzz);
+        // i.innerHTML = "fizz"
+    } else if (i % 5 === 0) { // per i multipli di 5 stampo "Buzz"
+        let buzz = "Buzz";
+        console.log(buzz);
+    } else if (i % 3 === 0) { // per i multipli di 3 stampa "Fizz"
+        let fizz = "Fizz";
+        console.log(fizz);
+    } else {
+        console.log(i);
     }
+
 }
 
 
 
 
-// per i multipli di 5 stampo "Buzz"
 
 
-// per i multipli di 3 e di 5 stampo "FizzBuzz"
+
